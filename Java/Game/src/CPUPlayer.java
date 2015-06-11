40b1433ef0ebb2eede86184c4c43c7cc3cb65265
the42guy <peter@alternateuniverse.cu.cc>
2015-06-11 09:54:57 +0530

    Lesser issues, cleaner code. Issues may arise sometimes.

--- Java/Game/src/CPUPlayer.java
+++ Java/Game/src/CPUPlayer.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import static java.lang.Math.*;
 
 /**
@@ -5,7 +7,7 @@ import static java.lang.Math.*;
  */
 public class CPUPlayer extends Player /*implements PlayerInterface*/ {                                                  //will implement when multiplayer is more developed, possibly not extend Player
     private static int score;
-    private static int unPredictabilityLen = 2;                                                                            //on a scale of 1-10, 10 means extremely difficult, 1 means darn easy
+    private static int unPredictabilityLen;                                                                            //on a scale of 1-10, 10 means extremely difficult, 1 means darn easy
     private static int[] range;
     private static String name = "Bot";
 
@@ -18,45 +20,41 @@ public class CPUPlayer extends Player /*implements PlayerInterface*/ {          
     }
 
     private static void setRanges(int length) {                                                                                   //randomly sets the ranges, depending on max capacity
-        range = new int[length];
-        System.out.printf("The length is %d, which is equal to %d\n", range.length, length);
-        for(int i = 0; i < range.length; i++) {
-            range[i] = (int) (random() * 10);
-            if ((range.length > 1) && (i > 0)) {                                                                //if the range length is more than 1 or not equal to 0
-                range[i] = createUniqueNumber(range[i], range);
-                System.out.printf("Range[%d] initialized as %d\n", i, range[i]);
-            } else if ((i == 0) || (range.length == 1)) {                                                                      //if the length of range is 1 or it is the first iteration here
+        ArrayList<Integer> range = new ArrayList<>(length);
+        System.out.printf("The length is %d\n", range.size());
+        for(int i = 0; i < length; i++) {
+            range.add(i, (int) (random() * 10));
+            if ((range.size() > 1) && (i > 0)) {                                                                //if the range length is more than 1 or not equal to 0
+                range.add(i, createUniqueNumber(range.get(i), range));
+                System.out.printf("Range[%d] initialized as %d\n", i, range.get(i));
+            } else if ((i == 0) || (range.size() == 1)) {                                                                      //if the length of range is 1 or it is the first iteration here
                 System.out.println("First time here/range length is one");
-                System.out.printf("range[%d] initialized as %d in this first iteration\n", i, range[i]);
+                System.out.printf("range[%d] initialized as %d in this first iteration\n", i, range.get(i));
             }
         }
         System.out.println("The range is now: ");
-        for(int i = 0; i < range.length; i++) {
-            if(i == range.length) {
-                System.out.print(range[i]);
+        for(int i = 0; i < length; i++) {
+            if(i == length) {
+                System.out.print(range.get(i));
             } else {
-                System.out.print(range[i] + ", ");
+                System.out.print(range.get(i) + ", ");
             }
 
         }
     }
 
-    private static int createUniqueNumber(int a, int[] arrayToSearchFora) {
-    	boolean areEqual = false;
-        for(int i = 0; i < arrayToSearchFora.length; i++) {
-            for(int j = 0; j < i; j++) {
-                boolean iAndJEqual = true;
-                do {
-                    if(range[i] == range[j]) {
-                        range[j] = (int) (Math.random() * 10);
-                        System.out.printf("The int %d now initialized as %d\n", range[i], range[j]);
-                    } else if (range[i] != range[j]) {
-                        System.out.printf("The ints %d and %d are not equal\n", range[i], range[j]);
-                        iAndJEqual = false;
-                    }
-                } while (iAndJEqual);
-                System.out.printf("Ending for inner index %d\n\n", j);
-            }
+    private static int createUniqueNumber(int a, ArrayList<Integer> arrayToSearchFora) {
+    	boolean areEqual = true;
+        for(int i = 0; i < arrayToSearchFora.size(); i++) {
+            do {
+                if(a == arrayToSearchFora.get(i)) {
+                    arrayToSearchFora.set(i, (int) (Math.random() * 10));
+                    System.out.printf("The int %d now initialized as %d, since they were equal\n", a, arrayToSearchFora.get(i));
+                } else if (a != arrayToSearchFora.get(i)) {
+                    System.out.printf("The ints %d and %d are not equal\n", a, arrayToSearchFora.get(i));
+                    areEqual = false;
+                }
+            } while (areEqual);
         }
 
         return a;
